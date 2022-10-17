97801ff89e0cc7f3b9d24d8c03dc8b9cdb821bb7
JessieDani <jessiedanielsr@gmail.com>
2022-10-17 12:22:41 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'
+export const TIMEOUT_SEC = 10
+export const RES_PER_PAGE = 10;
+export const KEY = 'd07dfa2c-9cfc-41ee-b19c-52af1ecd8be4';
+export const MODAL_CLOSE_SEC = 2.5;
